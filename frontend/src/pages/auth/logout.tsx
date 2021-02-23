e4b5b6efd6b4c890ab10002d5002d740bca53061
Paul Girard <paul@ouestware.com>
2021-02-23 23:56:35 +0100

    mimic logout

--- frontend/src/pages/auth/logout.tsx
+++ frontend/src/pages/auth/logout.tsx
@@ -1,15 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import { Redirect } from "react-router-dom";
 import { manager } from "../../hooks/user";
+import { AuthenticationContext} from "@axa-fr/react-oidc-context";
 
 interface Props {}
 export const PageLogout: React.FC<Props> = (props: Props) => {
-  useEffect(() => {
-    const main = async () => {
-      localStorage.removeItem("CURRENT_USER");
-      await manager.removeUser();
-    };
-    main();
-  });
+  const {userManager} = useContext(AuthenticationContext);
+  userManager.removeUser();
   return <Redirect to={"/"}></Redirect>;
 };
