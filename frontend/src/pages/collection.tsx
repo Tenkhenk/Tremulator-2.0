b6bb4188ba29b02edc9e689442222821a9b5cd1c
Paul Girard <paul@ouestware.com>
2021-03-09 12:04:04 +0100

    upload in modal

--- frontend/src/pages/collection.tsx
+++ frontend/src/pages/collection.tsx
@@ -4,6 +4,7 @@ import { AppContext } from "../context/app-context";
 import {useGet} from "../hooks/api";
 import {CollectionFullType, CollectionType, ImageType} from "../types/index";
 import ImageUpload from '../components/image-upload';
+import Modal from "../components/modal";
 
 interface Props {
     id:string
@@ -14,6 +15,7 @@ export const Collection: React.FC<Props> = (props:Props) => {
 const {id} = props;
 const {setAlertMessage, currentCollection, setCurrentCollection} = useContext(AppContext);
 const history = useHistory();
+const [isAddingPicture,setIsAddingPicture] = useState<Boolean>(false);
 const {data:getCollection, loading: loading, error: error} = useGet<CollectionFullType>(`/collections/${id}`);
 useEffect(()=>{
     if (getCollection){
@@ -30,7 +32,7 @@ const thumbnailURL= (iiifURL:string) => iiifURL.split('/').slice(0,-1).join('/')
 return <div className="container-fluid">
         {currentCollection && <>
             <div className="row">
-                <h3 className="col-4">{currentCollection.images.length} Pictures <button className='btn btn-primary btn-sm'>
+                <h3 className="col-4">{currentCollection.images.length} Pictures <button onClick={()=> setIsAddingPicture(true)} className='btn btn-primary btn-sm'>
                         <i className="far fa-plus-square  fa-2x" aria-label="add a picture" title="add a picture"></i>
                     </button></h3>
             </div>
@@ -39,9 +41,10 @@ return <div className="container-fluid">
                     <Link to={`/collections/${currentCollection.id}/images/${i.id}`}>{i.name} - {i.url} <img src={thumbnailURL(i.url)}/></Link>
                 </div>)}
             </div>
-            <div className="row">
+            {isAddingPicture && 
+            <Modal title="Add pictures" onClose={() => setIsAddingPicture(false)}>
                 <ImageUpload collection={currentCollection}></ImageUpload>
-            </div>
+            </Modal>}
             
         </>}
     </div>;
