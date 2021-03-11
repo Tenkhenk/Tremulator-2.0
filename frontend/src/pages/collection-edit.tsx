b1584ad80b983c51d9ed2c43c52a4731e1b5b0cf
Paul Girard <paul@ouestware.com>
2021-03-11 12:20:17 +0100

    confirm collection deletion

--- frontend/src/pages/collection-edit.tsx
+++ frontend/src/pages/collection-edit.tsx
@@ -5,6 +5,7 @@ import {CollectionType, CollectionFullType} from "../types/index";
 import {omit} from "lodash";
 import { AuthenticationContext } from "@axa-fr/react-oidc-context";
 import { AppContext } from "../context/app-context";
+import ModalPortal from "../components/modal";
 
 
 interface Props {
@@ -17,28 +18,29 @@ export const CollectionEdit: React.FC<Props> = (props:Props) => {
     // state management
     const {id} = props;
     const history= useHistory();
-    const {setAlertMessage} = useContext(AppContext);
+    const [needsConfirmation, setNeedsConfirmation] = useState<boolean>(false);
+    const {setAlertMessage, currentCollection: collection, setCurrentCollection: setCollection} = useContext(AppContext);
     const {oidcUser} = useContext(AuthenticationContext);
     const {data: getCollection, loading: getLoading, error: getError } = useGet<CollectionFullType>(`/collections/${id}`);
-    const [collection, setCollection] = useState<CollectionType|null>(null);
     const [putCollection, { loading: putLoading, error: putError }] = usePut<CollectionType, CollectionType>(`/collections/${id}`);
     const [deleteCollection, { loading: delLoading, error: delError }] = useDelete<CollectionType>(`/collections/${id}`);
 
     // initialize collection
     useEffect(() => {
         if (getCollection){
-            setCollection(omit(getCollection, ["schemas","images","users", "owner"]) as CollectionType)
+            setCollection(getCollection)
         }
-    },[getCollection])
+    },[getCollection, setCollection])
 
 
     // actions
-    const update = async (e:FormEvent)=> {
+    const updateCollection = async (e:FormEvent)=> {
         e.preventDefault();
 
         // modify an existing collection
         if (collection && collection.name !== '') {
-            await putCollection(collection);
+            await putCollection(omit(getCollection, ["schemas","images","users", "owner"]) as CollectionType)
+
             if (putError)
                 setAlertMessage({message:`Error when saving your collection "${putError.message}"`, type:"warning"})
             else
@@ -66,7 +68,7 @@ export const CollectionEdit: React.FC<Props> = (props:Props) => {
         <>
             <h1>{collection.name}</h1>
             {putError && putError.message}
-            <form onSubmit={update}>
+            <div>
                 <div className="fromGroup row">
                     <label htmlFor="name" className="col-sm-2 col-form-label">Name</label>
                     <div className="col-sm-6">
@@ -81,23 +83,36 @@ export const CollectionEdit: React.FC<Props> = (props:Props) => {
                 </div>
                 <div className="fromGroup row">
                     <div className="col-sm-8">
-                        <button className="btn btn-primary col-sm-2" type='submit' disabled={collection.name === ""}>{putLoading ? "loading..." : "save"}</button>
+                        <button className="btn btn-primary col-sm-2" onClick={(e)=>updateCollection(e)} disabled={collection.name === ""}>{putLoading ? "loading..." : "save"}</button>
                     </div>
                 </div>
-            </form>
-            <form onSubmit={del}>
+            </div>
+            {isOwner && <div>
                 <div className="fromGroup row">
                     <div className="col-sm-12 mt-5">
                         <h3><i className="fas fa-exclamation-triangle"></i> Danger Zone</h3>
-                        <p>If you delete a collection all the related images and annotations will be deleted too.</p>
+                        <p>If you delete this collection all the related {collection.images.length} images and their annotations will be deleted too.</p>
                     </div>
                 </div>
                 <div className="fromGroup row">
                     <div className="col-sm-12">
-                        <button className="btn btn-secondary" type='submit' disabled={collection.id === -1}>{putLoading ? "loading..." : "delete this collection"}</button>
+                        <button className="btn btn-secondary" onClick={() => (setNeedsConfirmation(true))} disabled={collection.id === -1}>{putLoading ? "loading..." : "delete this collection"}</button>
+                        
                     </div>
                 </div>
-            </form>  
+            </div> }
+            {needsConfirmation && <ModalPortal title="Confirmation needed" onClose={() => setNeedsConfirmation(false)}>
+                    <div className="h5">
+                        <div className="text-center">
+                            <i className="far fa-question-circle fa-2x mb-3"></i><br/>
+                            You are about to delete the collection<br/>"{collection.name}" and the {collection.images.length} associated images?
+                        </div>
+                        <div className="text-center mt-3">
+                            <button className="btn btn-secondary" onClick={()=> setNeedsConfirmation(false)}>Cancel <i className="fas fa-window-close ml-1"></i></button>
+                            <button className="btn btn-danger ml-2" onClick={(e) => del(e)}><i className="fas fa-trash-alt mr-1"></i> Delete</button>
+                        </div>
+                    </div>
+                </ModalPortal>}
         </>}
     </>
 };
