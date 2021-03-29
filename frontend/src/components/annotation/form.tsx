ec5463b54b40a71c1729778e1cb5e50e898b7c4a
Benoit Simard <contact@bsimard.com>
2021-03-29 09:26:37 +0200

    [frontend]: Correct loader on annotation form

--- frontend/src/components/annotation/form.tsx
+++ frontend/src/components/annotation/form.tsx
@@ -44,90 +44,94 @@ export const AnnotationForm: FC<Props> = (props: Props) => {
 
   return (
     <>
-      {createLoading || (updateLoading && <Loader />)}
-      {(withTitle === undefined || withTitle === true) && (
-        <div className="row">
-          <div className="col">
-            <h3>
-              <i className="fas fa-vector-square mr-1"></i>
-              {annotation.id < 0 ? "New annotation" : `Annotation #${annotation.id}`}
-            </h3>
-          </div>
-        </div>
-      )}
-
-      {annotation.id < 0 && schemas.length > 1 && (
-        <div className="row">
-          <div className="col">
-            <div className="form-group">
-              <div className="form-group">
-                <select
-                  id="schema"
-                  className="custom-select"
-                  onChange={(e) => setSchema(schemas.find((item) => "" + item.id === e.target.value) || null)}
-                >
-                  {schemas.map((schema) => (
-                    <option key={schema.id} value={schema.id}>
-                      Schema: {schema.name}
-                    </option>
-                  ))}
-                </select>
+      {(createLoading || updateLoading) && <Loader />}
+      {!(createLoading || updateLoading) && (
+        <>
+          {(withTitle === undefined || withTitle === true) && (
+            <div className="row">
+              <div className="col">
+                <h3>
+                  <i className="fas fa-vector-square mr-1"></i>
+                  {annotation.id < 0 ? "New annotation" : `Annotation #${annotation.id}`}
+                </h3>
               </div>
             </div>
-          </div>
-        </div>
-      )}
+          )}
 
-      {schema && (
-        <div className="row">
-          <div className="col">
-            <Form
-              schema={schema.schema}
-              uiSchema={schema.ui}
-              formData={annotation.data}
-              onSubmit={async (e) => {
-                try {
-                  let data = omit({ ...annotation, data: e.formData }, [
-                    "schema_id",
-                    "image_id",
-                    "created_at",
-                    "updated_at",
-                  ]);
-                  if (annotation.id < 0) {
-                    data = await create(data, {
-                      schemaId: schema.id,
-                    });
-                    setAlertMessage({ message: `Annotation created`, type: "success" });
-                  } else {
-                    await update(data);
-                    setAlertMessage({ message: `Annotation updated`, type: "success" });
-                  }
-                  if (onSaved) onSaved(data.id);
-                } catch (error) {
-                  setAlertMessage({
-                    message: `Error when creating annotation "${error?.message}" created`,
-                    type: "warning",
-                  });
-                }
-              }}
-            >
-              <div className="form-group text-right">
-                <button
-                  type="reset"
-                  className="btn btn-secondary  ml-2"
-                  onClick={() => {
-                    if (onCancel) onCancel();
+          {annotation.id < 0 && schemas.length > 1 && (
+            <div className="row">
+              <div className="col">
+                <div className="form-group">
+                  <div className="form-group">
+                    <select
+                      id="schema"
+                      className="custom-select"
+                      onChange={(e) => setSchema(schemas.find((item) => "" + item.id === e.target.value) || null)}
+                    >
+                      {schemas.map((schema) => (
+                        <option key={schema.id} value={schema.id}>
+                          Schema: {schema.name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+
+          {schema && (
+            <div className="row">
+              <div className="col">
+                <Form
+                  schema={schema.schema}
+                  uiSchema={schema.ui}
+                  formData={annotation.data}
+                  onSubmit={async (e) => {
+                    try {
+                      let data = omit({ ...annotation, data: e.formData }, [
+                        "schema_id",
+                        "image_id",
+                        "created_at",
+                        "updated_at",
+                      ]);
+                      if (annotation.id < 0) {
+                        data = await create(data, {
+                          schemaId: schema.id,
+                        });
+                        setAlertMessage({ message: `Annotation created`, type: "success" });
+                      } else {
+                        await update(data);
+                        setAlertMessage({ message: `Annotation updated`, type: "success" });
+                      }
+                      if (onSaved) onSaved(data.id);
+                    } catch (error) {
+                      setAlertMessage({
+                        message: `Error when creating annotation "${error?.message}" created`,
+                        type: "warning",
+                      });
+                    }
                   }}
                 >
-                  Cancel
-                </button>
-                <button type="submit" className="btn btn-primary  ml-2">
-                  Submit
-                </button>
+                  <div className="form-group text-right">
+                    <button
+                      type="reset"
+                      className="btn btn-secondary  ml-2"
+                      onClick={() => {
+                        if (onCancel) onCancel();
+                      }}
+                    >
+                      Cancel
+                    </button>
+                    <button type="submit" className="btn btn-primary  ml-2">
+                      Submit
+                    </button>
+                  </div>
+                </Form>
               </div>
-            </Form>
-          </div>
-        </div>
+            </div>
+          )}
+        </>
       )}
     </>
   );
